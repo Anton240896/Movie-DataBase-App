b392bac88ca84e8fe26b8270f559ebaf931ea9ca
Anton240896 <antonalipov791@gmail.com>
2024-02-29 11:13:37 +0200

    add rename project

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { Toaster } from 'react-hot-toast';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/goit-react-hw-05-movies">
+    <BrowserRouter basename="/Movie-DataBase-App">
       <App />
       <GlobalStyles />
       <Toaster position="top-right" />
